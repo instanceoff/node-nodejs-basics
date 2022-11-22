ccb585f8b264960c815f6cb1e1cdb00197b52295
Ilya Khinevich <ilyaxinevich@gmail.com>
2022-11-22 09:28:14 +0300

    feat: copy.js functionality

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,19 @@
+import * as fs from 'fs';
+
 const copy = async () => {
-    // Write your code here 
+  try {
+    if (fs.existsSync('src/fs/files_copy')) throw 'FS operation failed';
+
+    fs.mkdir('src/fs/files_copy', (err) => {
+      if (err) throw 'FS operation failed';
+    });
+
+    fs.cp('src/fs/files', 'src/fs/files_copy', { recursive: true }, (err) => {
+      if (err) throw 'FS operation failed';
+    });
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 copy();
