7e1243baa430ed50cfa858a877e99a5262405727
Ilya Khinevich <ilyaxinevich@gmail.com>
2022-12-02 08:44:37 +0300

    feat: list functionality

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,12 @@
+import * as fs from 'fs';
+
 const list = async () => {
-    // Write your code here 
+  try {
+    const dir = fs.opendir('src/fs/files');
+    for await (const dirent of dir) console.log(dirent.name);
+  } catch (err) {
+    console.error('FS operation failed');
+  }
 };
 
 await list();
